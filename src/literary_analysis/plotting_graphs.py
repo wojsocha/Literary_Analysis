40bcde9019c5852c1356613d018641255228f2bc
Wojciech Socha <wojteksocha02@gmail.com>
2026-09-05 22:37:53 +0200

    Extended the notebook, including creating a new function for plotting_graphs.py that draws a heat map, with test.

--- src/literary_analysis/plotting_graphs.py
+++ src/literary_analysis/plotting_graphs.py
@@ -9,7 +9,18 @@ def plot_similarities(similarity_matrix, work_names, work_index):
     ax.bar(names, scores)
     ax.set_ylim(0, 100)
     ax.set_xlabel("Compared works")
+    ax.xaxis.set_label_coords(-0.1, -0.05)
+    ax.tick_params(axis="x", labelrotation=90)
     ax.set_ylabel("Level of similarity")
     ax.set_title(f"How similar is {work_names[work_index]} to other works")
     plt.show()
     return ax #This is only for testing purposes
+
+def plot_matrix(similarity_matrix, all_names, how):
+    fig, ax = plt.subplots()
+    ax.imshow(similarity_matrix, cmap=how)
+    ax.set_xticks(range(len(all_names)), all_names, rotation=90)
+    ax.set_yticks(range(len(all_names)), all_names)
+    ax.set_title(f"Heatmap of how similar are Master's works to each other")
+    plt.show()
+    return ax
--- tests/test_plotting_graphs.py
+++ tests/test_plotting_graphs.py
@@ -1,5 +1,5 @@
 import pytest
-from literary_analysis.plotting_graphs import plot_similarities
+from literary_analysis.plotting_graphs import plot_similarities, plot_matrix
 
 @pytest.fixture
 def matrix():
@@ -9,11 +9,22 @@ def matrix():
 def names():
     return ["n1", "n2", "n3"]
 
-def test_plot(matrix, names, monkeypatch):
+def test_plot_sim(matrix, names, monkeypatch):
     monkeypatch.setattr("matplotlib.pyplot.show", lambda: None)
     ax = plot_similarities(matrix, names, 1)
     heights = [bar.get_height() for bar in ax.patches]
     labels = [label.get_text() for label in ax.get_xticklabels()]
+
     assert heights == [2, 6]
     assert labels == ["n1", "n3"]
     assert ax.get_ylim() == (0.0, 100.0)
+
+def test_plot_mtx(matrix, names, monkeypatch):
+    monkeypatch.setattr("matplotlib.pyplot.show", lambda: None)
+    ax = plot_matrix(matrix, names, "cool")
+    x_labels = [label.get_text() for label in ax.get_xticklabels()]
+    y_labels = [label.get_text() for label in ax.get_yticklabels()]
+
+    assert x_labels == names
+    assert y_labels == names
+    assert all(label.get_rotation() == 90 for label in ax.get_xticklabels())
